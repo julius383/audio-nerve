02af9d732b387a80ce69f9835f29eac2e59a6e4e
Kenneth Gitere <gitere81@gmail.com>
2020-05-12 22:15:39 +0300

    Add logging utility and import packages for assisting in development such as nodemon

---
+++ src/util/logger.ts
@@ -0,0 +1,16 @@
+import * as winston from "winston";
+const { printf, timestamp, combine } = winston.format;
+const logFormat = printf(({ level, message, timestamp: time }) => {
+  return `${level.toUpperCase()}: ${time}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: "debug",
+      format: combine(timestamp(), logFormat),
+    }),
+  ],
+});
+
+export default logger;
