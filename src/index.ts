579cb0824ef056ecee5f58c5747594f3e545251d
Kenneth Gitere <gitere81@gmail.com>
2020-05-17 22:13:11 +0300

    Refactor authentication routes

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,16 @@ import logger from "./util/logger";
 import { router as authRouter } from "./routes/auth";
 
 const PORT: string = process.env.PORT || "8000";
+
 const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello from Audio Nerve");
+  res.send("Hello from Audio Nerve");
 });
 
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, () => {
-    logger.debug(`Started app on http://localhost:${PORT}`);
+  logger.debug(`Started app on http://localhost:${PORT}`);
 });
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,107 +1,161 @@
-import { PrismaClient } from "@prisma/client"
-import { Router, Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
+import {
+  Router,
+  Request,
+  Response,
+  NextFunction,
+  ErrorRequestHandler,
+} from "express";
+import expressJwt from "express-jwt";
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
-import passport from "passport";
-import { Strategy as LocalStrategy } from "passport-local";
 import jwt from "jsonwebtoken";
 
 import logger from "../util/logger";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 const router = Router();
 const ROUNDS = 10;
-const JWT_SECRET = "SECretk3y"
+const JWT_SECRET = "SECretk3y";
+const TOKEN_EXP = 60 * 15;
 
+const generateJWT = (email: string, id: string, expSec: number): string => {
+  const now = new Date();
+  const exp = new Date(now);
+  exp.setSeconds(now.getSeconds() + expSec);
+  return jwt.sign(
+    {
+      id,
+      email,
+      exp: exp.getTime() / 1000,
+      iat: now.getTime() / 1000,
+    },
+    JWT_SECRET
+  );
+};
 
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-},
-    async (email, password, done) => {
-        try {
-            const user = await prisma.user.findOne({ where: { email } })
-            if (user !== null) {
-                const isSamePassword = await bcrypt.compare(password, user.password);
-                if (isSamePassword) {
-                    done(null, user)
-                } else {
-                    done(null, false, { message: 'Incorrect email or password' });
-                }
-            } else {
-                done(null, false, { message: "Account does not exist" })
-            }
-        } catch (err) {
-            logger.error(JSON.stringify(err))
-            done(err)
-        }
-    }
-))
+const getToken = (req: Request): string | null => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    return req.headers.authorization.split(" ")[1];
+  }
+  return null;
+};
 
-const generateJWT = (email: string, id: string): string => {
-    const now = new Date();
-    const expiresIn = new Date(now)
-    expiresIn.setDate(now.getDate() + 7)
-    return jwt.sign({
-        id,
-        email,
-        exp: expiresIn.getTime() / 1000,
-        iat: now.getTime() / 1000
-    }, JWT_SECRET)
+interface DecodedToken {
+  user: { id: string; email: string };
+}
+
+interface ErrorObj {
+  name: string;
+  message: string;
 }
 
 router.post("/register", async (req: Request, res: Response) => {
-    const { name, email, password } = req.body
-    try {
-        const hashPassword = await bcrypt.hash(password, ROUNDS);
-        await prisma.user.create({
-            data: {
-                id: uuidv4(),
-                name,
-                email,
-                password: hashPassword,
-            }
-        })
-        res.json({ "msg": "User created successfully" })
-    } catch (err) {
-        // ad-hoc error handling given there is no way of using types to ensure proper error handling
-
-        if (err?.meta?.target?.includes("email")) {
-            res.status(400).json({ "validation_error": "This email is already taken" })
-        } else {
-            logger.error(JSON.stringify(err))
-            res.sendStatus(500)
-        }
-
+  const { name, email, password } = req.body;
+  if (!name) {
+    res.status(400).json({ error: "Name is required" });
+  } else if (!email) {
+    res.status(400).json({ error: "Email is required" });
+  } else if (!password) {
+    res.status(400).json({ error: "Password is required" });
+  }
+  try {
+    const hashPassword = await bcrypt.hash(password, ROUNDS);
+    const isExistingEmail = await prisma.user.findOne({ where: { email } });
+    if (isExistingEmail) {
+      res.status(400).json({ error: "This email is already taken" });
+    } else {
+      await prisma.user.create({
+        data: {
+          id: uuidv4(),
+          name,
+          email,
+          password: hashPassword,
+        },
+      });
+      res.status(201).json({ msg: "User created successfully" });
     }
+  } catch (err) {
+    logger.error(JSON.stringify(err));
+    res.sendStatus(500);
+  }
+});
 
-})
-
-router.post("/login", async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password } = req.body;
-    if (!email) {
-        res.status(400).json({
-            error: "Email is required"
-        })
-    } else if (!password) {
-        res.status(400).json({
-            error: "Password is required"
-        })
+router.post("/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email) {
+    res.status(400).json({
+      error: "Email is required",
+    });
+  } else if (!password) {
+    res.status(400).json({
+      error: "Password is required",
+    });
+  }
+  try {
+    const user = await prisma.user.findOne({ where: { email } });
+    if (user !== null) {
+      const isSamePassword = await bcrypt.compare(password, user.password);
+      if (isSamePassword) {
+        const token = generateJWT(user.email, user.id, TOKEN_EXP);
+        res.json({ token });
+      } else {
+        res.status(400).json({ error: "Incorrect email or password" });
+      }
+    } else {
+      res.status(400).json({ error: "Account does not exist" });
     }
-    return passport.authenticate('local', { session: false }, async (err, user, info) => {
-        if (err) {
-            res.sendStatus(500)
-        } else if (user) {
-            const token = generateJWT(user.email, user.id)
-            res.json({ token })
-        } else {
-            res.status(400).json({ error: info })
-        }
-    })(req, res, next)
-})
+  } catch (err) {
+    logger.error(JSON.stringify(err));
+    res.sendStatus(500);
+  }
+});
 
 router.post("/logout", (_req: Request, res: Response) => {
-    res.status(200).end()
-})
+  res.status(200).end();
+});
 
-export { router }
+router.get(
+  "/user",
+  expressJwt({ secret: JWT_SECRET, getToken }),
+  async (req: Request, res: Response) => {
+    const { user: token } = (req as object) as DecodedToken;
+    if (token) {
+      const user = await prisma.user.findOne({
+        where: { email: token.email },
+      });
+      if (user != null) {
+        res.json({
+          name: user.name,
+          email: user.email,
+        });
+      } else {
+        res.status(401).json({ error: "Invalid token" });
+      }
+    } else {
+      res.sendStatus(400);
+    }
+  }
+);
+
+router.use(
+  (
+    err: ErrorRequestHandler,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+  ) => {
+    logger.debug(JSON.stringify(err));
+    if (err.name === "UnauthorizedError") {
+      const errorObj = (err as object) as ErrorObj;
+      const errorMsg =
+        errorObj.message === "jwt expired" ? "Expired token" : "Invalid token";
+      res.status(401).json({ error: errorMsg });
+    }
+  }
+);
+
+export { router };
